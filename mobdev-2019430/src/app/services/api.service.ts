0f40c0720ecdb6f3dc255a2f7c3ce272923fa0b0
Miriam Silva Araujo <miriam.design@live.com>
2020-04-16 11:52:58 +0000

    More changes

--- mobdev-2019430/src/app/services/api.service.ts
+++ mobdev-2019430/src/app/services/api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
-  constructor() { }
+ 
+  constructor(private http: HttpClient) { }
+ 
+  getEpisodes() {
+    return this.http.get('https://breakingbadapi.com/');
+  }
+ 
+  getEpisode(id) {
+    return this.http.get(`https://breakingbadapi.com/api/episodes/${id}`);
+  }
 }

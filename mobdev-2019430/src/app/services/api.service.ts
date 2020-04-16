4880544f92b2db8f425f46829a589eec94222612
Miriam Silva Araujo <miriam.design@live.com>
2020-04-16 16:08:04 +0000

    peekaboo

--- mobdev-2019430/src/app/services/api.service.ts
+++ mobdev-2019430/src/app/services/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient} from '@angular/common/http'
  
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,7 @@ export class ApiService {
   constructor(private http: HttpClient) { }
  
   getEpisodes() {
-    return this.http.get('https://breakingbadapi.com/');
+    return this.http.get('https://breakingbadapi.com/episodes');
   }
  
   getEpisode(id) {

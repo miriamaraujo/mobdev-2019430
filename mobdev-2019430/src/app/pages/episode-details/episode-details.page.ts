9812ea86c88032352e68f681f852c5762a5bcb56
Miriam Silva Araujo <miriam.design@live.com>
2020-04-16 11:22:22 +0000

    SayMyName

---
+++ mobdev-2019430/src/app/pages/episode-details/episode-details.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+ 
+@Component({
+  selector: 'app-episode-details',
+  templateUrl: './episode-details.page.html',
+  styleUrls: ['./episode-details.page.scss'],
+})
+export class EpisodeDetailsPage implements OnInit {
+ 
+  episodeId = null;
+ 
+  constructor(private activatedRoute: ActivatedRoute) { }
+ 
+  ngOnInit() {
+    this.episodeId = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+ 
+}
